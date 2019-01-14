8cab5a086019a54eed32e5bc712c993d417d0814
Shashanka Shekhar Nayak <44174922+Shashanka8@users.noreply.github.com>
2019-01-14 23:11:36 +0530

    First Commit

---
+++ src/app/app.module.ts
@@ -0,0 +1,33 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppRoutingModule } from './app-routing.module';
+import { ReactiveFormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
+import { SlimLoadingBarModule } from 'ng2-slim-loading-bar';
+
+import { AppComponent } from './app.component';
+import { GstAddComponent } from './gst-add/gst-add.component';
+import { GstGetComponent } from './gst-get/gst-get.component';
+import { GstEditComponent } from './gst-edit/gst-edit.component';
+
+import { BusinessService } from './business.service';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    GstAddComponent,
+    GstGetComponent,
+    GstEditComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    SlimLoadingBarModule,
+    ReactiveFormsModule,
+    HttpClientModule
+  ],
+  providers: [ BusinessService ],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
